2f953787755031f0512bd5bfae1e52a5da22c075
aalichao <aalichao@ucsc.edu>
2025-07-08 16:54:36 -0700

    changing module.mk

--- apps/demos/nnse_usb_ota/module.mk
+++ apps/demos/nnse_usb_ota/module.mk
@@ -6,11 +6,7 @@ local_src += $(wildcard $(subdirectory)/src/*.s)
 local_bin := $(BINDIR)/$(subdirectory)
 
 bindirs   += $(local_bin)
+# sources   += $(local_src)
 examples  += $(local_bin)/$(local_app_name).axf
 examples  += $(local_bin)/$(local_app_name).bin
-# # mains 	+= $(wildcard $(subdirectory)/src/*.o)
-# # mains     += $(local_bin)/src/$(local_app_name).o
-# mains     += $(wildcard  $(local_bin)/src/*.o)
-$(eval $(call make-axf, $(local_bin)/$(local_app_name), $(local_src)))
-
-includes_api += $(PROJECT_ROOT)/extern/tensorflow/ns_tflm_v1_0_0/third_party/flatbuffers/include
+$(eval $(call make-axf, $(local_bin)/$(local_app_name), $(local_src), $(local_cflags)))
